c2238fbc7710b4ea427a672756c0407af20bd3b0
AnnaChrone <jnchronis1@gmail.com>
2026-04-17 14:51:18 +0200

    Fixed the bugs within getbest.py

--- getbest.py
+++ getbest.py
@@ -5,20 +5,27 @@ import sys
 def getCols(f):
     ''' Identify the columns that contain the marks and student numbers '''
     headings = f.readline().strip().split(",")
-    i=1
-    for head in headings:
+    num_col = None #FIX: starts index from 0
+    mark_col = None
+
+    for i,head in enumerate(headings): #FIX: allows for correct indexing
         if head == "Student Number": num_col=i
         elif head == "Mark" : mark_col = i
     return (num_col, mark_col)
 
 def findTop(f,num_col, mark_col):
     ''' finds the top student in the class '''
-    best = best_idx =  0
+    best = None
+    best_idx = None
     for line in f:
         data = line.strip().split(",")
+        if len(data) <= max(num_col, mark_col):
+            continue
+
         mark = int(data[mark_col])
-        if mark > best:
-            best=mark
+        if best is None or mark > best:
+            best = mark
+            best_idx = data[num_col] #FIX: best_idx was not being updated
     return best_idx, best
 
 f = open(sys.argv[1])
